5c0f455758a346a3055f47099beb33ab02932c17
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2025-03-11 13:44:17 -0300

    feat: Adicionada nova rota no gateway

--- mscloudgateway/src/main/java/io/github/springcloud/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/springcloud/mscloudgateway/MscloudgatewayApplication.java
@@ -17,9 +17,11 @@ public class MscloudgatewayApplication {
 
 	@Bean
 	public RouteLocator routes(RouteLocatorBuilder builder){
-		return builder
-				.routes()
-					.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+		return builder.routes()
+				.route(r -> r.path("/clientes/**")
+						.uri("lb://msclientes"))
+				.route(r -> r.path("/cartoes/**")
+						.uri("lb://mscartoes"))
 				.build();
 	}
 
