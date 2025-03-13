7dc4a4298b2705fdff2029cb66b9ccc0c0404258
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2025-03-13 07:33:51 -0300

    feat: Adicionado endpoints de Buscar por Renda(getRendaAteh) e Buscar Cartões Por Cliente(getCartoesByClienteCpf)

--- mscartoes/src/main/java/io/github/springcloud/mscartoes/web/controller/CartoesController.java
+++ mscartoes/src/main/java/io/github/springcloud/mscartoes/web/controller/CartoesController.java
@@ -2,11 +2,15 @@ package io.github.springcloud.mscartoes.web.controller;
 
 import io.github.springcloud.mscartoes.domain.Cartao;
 import io.github.springcloud.mscartoes.domain.CartaoService;
+import io.github.springcloud.mscartoes.domain.clienteCartao.ClienteCartaoService;
 import io.github.springcloud.mscartoes.web.dto.request.CartaoRequest;
+import io.github.springcloud.mscartoes.web.dto.response.CartoesPorClienteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cartoes")
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class CartoesController {
 
     private final CartaoService cartaoService;
+    private final ClienteCartaoService clienteCartaoService;
 
     @GetMapping
     public String status(){
@@ -25,4 +30,22 @@ public class CartoesController {
         Cartao cartao = cartaoService.save(cartaoRequest.toEntity());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getRendaAteh(@RequestParam("renda") Long renda){
+        List<Cartao> list = cartaoService.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<List<CartoesPorClienteResponse>> getCartoesByClienteCpf(@RequestParam("cpf") String cpf){
+        List<CartoesPorClienteResponse> cartoes = clienteCartaoService.listarCartoesPorCpf(cpf)
+                .stream()
+                .map(clienteCartao -> new CartoesPorClienteResponse(clienteCartao.getCartao().getNome(),
+                        clienteCartao.getCartao().getBandeira().name(),
+                        clienteCartao.getLimite()))
+                .toList();
+        return ResponseEntity.ok(cartoes);
+    }
+
 }
